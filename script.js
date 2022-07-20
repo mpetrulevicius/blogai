ce77fc4bbebdb86faab915f065c9e114ba27e355
mpetrulevicius <106998029+mpetrulevicius@users.noreply.github.com>
2022-07-20 21:45:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// labai liudnai. tikiuosi perprasiu iki 2adienio kazkiek daugiau.
+
+// do {
+//     var kaina = prompt("Prašau įveskite prakės kainą")
+//     kaina = Number(kaina)
+// } while(isNaN(kaina) || kaina <= 0)
+
+
+// do {
+//     var pristatymas = prompt("Ar reikalingas pristatymas į namus?")
+//     pristatymas = pristatymas(pristatymas)
+// } while(isNaN(pristatymas) || kaina <= 0)
+
+
+// var taip = pristatymas 
+// var ne = pristatymas 
+
+// console.log("taip ")
+// console.log("ne ")
+
+
+
+
